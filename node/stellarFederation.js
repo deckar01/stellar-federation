6481c0cdfefafcb9f527aa55b682f1ee380342f5
Greg Brockman <gdb@gregbrockman.com>
2014-08-05 11:25:35 -0700

    Flesh out federation a bit more
    - Add CORS headers
    - Make it work with and without native SSL

--- node/stellarFederation.js
+++ node/stellarFederation.js
@@ -1,21 +1,29 @@
+// Simple Stellar federation server.
+//
+// You may have to `npm i express` to install dependencies.
+
 // Configure federation
 var DOMAIN_NAME = 'example.com';
 var STELLAR_ADDRESS = 'Stellar address goes here';
 
-// Configure SSL
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+// Configure SSL, if you'd like to terminate it in Node. For testing
+// without your own SSL certificate, use https://ngrok.com/ to set up
+// an SSL-secured localtunnel.
+var USE_SSL = false;
+var PRIVATE_KEY = 'sslcert/server.key';
+var PUBLIC_KEY = 'sslcert/server.crt';
 
 // Include dependencies
 var express = require('express');
 var https = require('https');
+var fs = require('fs');
 
 // Create the server
 var app = express();
-var server = https.createServer({key: privateKey, cert: certificate}, app);
 
 // Accept federation requests
-app.get('/federation', function(req, res) {
+app.get('/stellar/federation', function(req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send({
     federation_json: {
       type:                'federation_record',
@@ -28,9 +36,17 @@ app.get('/federation', function(req, res) {
 
 // Accept federation URL requests
 app.get('/stellar.txt', function(req, res) {
-    res.set('Content-Type', 'text/plain');
-    res.send('[federation_url]\nhttp://' + DOMAIN_NAME + '/federation');
+  res.set('Content-Type', 'text/plain');
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send('[federation_url]\nhttps://' + DOMAIN_NAME + '/stellar/federation');
 });
 
-// Start the server
-server.listen(443);
+if (USE_SSL) {
+  var privateKey  = fs.readFileSync(PRIVATE_KEY, 'utf8');
+  var certificate = fs.readFileSync(PUBLIC_KEY, 'utf8');
+
+  var server = https.createServer({key: privateKey, cert: certificate}, app);
+  server.listen(5000);
+} else {
+  app.listen(5000);
+}
